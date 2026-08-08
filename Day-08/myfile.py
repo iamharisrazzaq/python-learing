2015e2a106c51d06786485f9c4d9bba5b86c8a8c
iamharisrazzaq-max <iamharisrazzaq@gmail.com>
2026-08-08 16:10:18 +0500

    Message

---
+++ Day-08/myfile.py
@@ -0,0 +1,4 @@
+print("hello world")
+print("i am a good boy and\n i am a programer")
+# this is comment
+print("my name is haris\'Haris my personal\'\n and  i am learning python language ")
--- day-7/master.py
+++ day-7/master.py
@@ -6,3 +6,8 @@ print("Product Name:", product_name)
 print("Price:", price)
 print("Quantity:", quantity)
 print("Available:", available)
+
+a = int(input("enter something: "))
+print(type(a))
+b=77
+print(type(b))
